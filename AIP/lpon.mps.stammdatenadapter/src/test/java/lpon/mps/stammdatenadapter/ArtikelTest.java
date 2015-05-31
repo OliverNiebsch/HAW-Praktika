2a4c3e3df715a484b99f55d6906d77be4b8d263a
OliverNiebsch <oliver.niebsch@haw-hamburg.de>
2015-05-31 18:45:04 +0200

    Ersten Test erstellt

---
+++ AIP/lpon.mps.stammdatenadapter/src/test/java/lpon/mps/stammdatenadapter/ArtikelTest.java
@@ -0,0 +1,49 @@
+package lpon.mps.stammdatenadapter;
+
+import java.util.ArrayList;
+
+import lpon.mps.stammdatenadapter.entities.Artikel;
+import lpon.mps.stammdatenadapter.services.ArtikelService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=lpon.mps.stammdatenadapter.ArtikelTest.ContextConfiguration.class)
+public class ArtikelTest {
+
+	@ComponentScan(basePackages = "lpon.mps.stammdatenadapter.services")
+	@Configuration
+	static class ContextConfiguration {}
+	
+	@Autowired
+	private ArtikelService artikelService;
+	
+	@Test
+	public void createArtikel() {
+		Artikel a1 = new Artikel("Schraube", null);
+		Assert.isNull(a1.getId());
+		artikelService.saveArtikel(a1);
+		Assert.notNull(a1.getId());
+		
+		Artikel a2 = new Artikel("Mutter", null);
+		Assert.isNull(a2.getId());
+		artikelService.saveArtikel(a2);
+		Assert.notNull(a2.getId());
+		
+		ArrayList<Artikel> artikelListe = new ArrayList<Artikel>();
+		artikelListe.add(a1);
+		artikelListe.add(a2);
+		
+		Artikel a3 = new Artikel("Rasenmäher", artikelListe);
+		Assert.isNull(a3.getId());
+		artikelService.saveArtikel(a3);
+		Assert.notNull(a3.getId());
+	}
+}
---
+++ AIP/lpon.mps.stammdatenadapter/src/test/java/lpon/mps/stammdatenadapter/DatabaseConfig.java
@@ -0,0 +1,9 @@
+package lpon.mps.stammdatenadapter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+// this allows us to configure our database via the application.properties file
+public class DatabaseConfig {}
