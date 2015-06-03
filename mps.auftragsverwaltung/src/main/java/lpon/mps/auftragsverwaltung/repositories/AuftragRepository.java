1d2403449df3f9637f1c3c2835e625a7580476f4
OliverNiebsch <oliver.niebsch@haw-hamburg.de>
2015-06-03 15:42:56 +0200

    Alte Dateien gelöscht

--- mps.auftragsverwaltung/src/main/java/lpon/mps/auftragsverwaltung/repositories/AuftragRepository.java
+++
@@ -1,9 +0,0 @@
-package lpon.mps.auftragsverwaltung.repositories;
-
-import lpon.mps.auftragsverwaltung.entities.Auftrag;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface AuftragRepository extends PagingAndSortingRepository<Auftrag, Long> {
-	
-}
--- mps.ppsadapter/src/main/java/lpon/mps/ppsadapter/PPSAdapter.java
+++
@@ -1,9 +0,0 @@
-package lpon.mps.ppsadapter;
-
-import lpon.mps.stammdatenadapter.entities.Artikel;
-
-public class PPSAdapter {
-	public void bauteilfertigen(Artikel bauteil) {
-		// TODO: Implementieren
-	}
-}
