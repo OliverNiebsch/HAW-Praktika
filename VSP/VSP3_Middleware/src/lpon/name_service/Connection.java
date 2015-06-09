5d9b4487a6dfa65574c13011f0ee086b39d1d91a
Lars.Porep@porep.de <Lars.Porep@porep.de>
2015-06-09 15:22:01 +0400

    Nameserver

--- VSP/VSP3_Middleware/src/lpon/name_service/Connection.java
+++ VSP/VSP3_Middleware/src/lpon/name_service/Connection.java
@@ -1,33 +1,157 @@
 package lpon.name_service;
 
+import java.awt.image.LookupTable;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.lang.invoke.MethodHandles.Lookup;
 import java.net.Socket;
 
 public class Connection extends Thread {
 	private Socket listeningSocket;
-
-	public Connection(Socket listeningSocket) {
+	private InputStreamReader reader = null;  //new InputStreamReader(socket.getInputStream());
+	private OutputStreamWriter writer = null; //new OutputStreamWriter(socket.getOutputStream());
+	private LookUpTable lookUpTable = null;
+	private boolean closeConnection = false;
+	
+	private static final String CRLF = "\n";
+	
+	public Connection(Socket listeningSocket, LookUpTable lookUpTable) {
 		super();
 		this.listeningSocket = listeningSocket;
+		this.lookUpTable = lookUpTable;
+	}
+	
+	
+	private void writeLine(String line) {
+		try {
+			System.out.println(line);
+			writer.write(line + CRLF);
+			writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		super.run();
-		
+
 		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(listeningSocket.getInputStream()));
-			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(listeningSocket.getOutputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(listeningSocket.getInputStream()));
 			
-			in.close();
-			out.close();
+			
+			writer = new OutputStreamWriter(listeningSocket.getOutputStream());
+			
+			while(!closeConnection){
+				//TODO do stuff
+				String inputMessage = reader.readLine();
+				writeLine((handleInputMessage(inputMessage)));//SWITCH COMMANDS
+				
+			}
+			//reader.close();
+			//SOCKET:close
+			// in.close();
+			// out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	private String handleInputMessage(String inputMessage) {
+		
+		String commandType;
+		String value;
+		
+		if(!inputMessage.contains(",")){
+			commandType = "BAD_COMMAND";
+			value = "inputMessage";
+		}
+		
+		else{
+			int firstSpace = inputMessage.indexOf(",");
+			
+			commandType = inputMessage.substring(0, firstSpace-1);
+			value = inputMessage.substring(firstSpace+1);
+		}
+		
+		//String firstCommand = inputMessage.indexOf(" ");
+		
+		String result = "";
+
+		switch (commandType) {
+		case "BAD_COMMAND":
+			result = "UnknownCommand received: " + inputMessage;
+			break;
+		case "rebind":
+			//TODO rebind myHost, myPort, name
+			String[] ary = value.split(",");
+			
+			if(ary.length != 3){
+				String myHost = ary[0];
+				int myPort = Integer.parseInt(ary[1]);
+				String name = ary[2];
+				lookUpTable.rebind(name, myHost, myPort);
+				result = "rebind_reply ok"; 	
+			}
+			else{
+				result = "rebind_reply failed: RECEIVED:" + inputMessage;
+				
+			}
+			break;
+		case "resolve":
+			ResolveObject resObj = lookUpTable.resolve(value);
+			String myHost = resObj.getHostname();
+			String myPort = resObj.getPort() + "";
+			String name = resObj.getId();
+			result = "resolve_reply " + name + "," + myHost + "," + myPort;
+			break;
+			
+		default:
+			result = "COULD NOT HANDLE:: " + inputMessage;
+			//throw new RuntimeException(); // - > really bad status
+			// break;
+		}
+
+		return result;
+	}
+	
+	public void closeConnection() {
+		// TODO
+	}
+
+	/*
+	private String readLine() {
+		if (reader == null)
+			return null;
+
+		StringBuilder line = new StringBuilder();
+		char c = 'a';
+		boolean cr = false;
+
+		try {
+			while (true) {
+				c = (char) reader.read();
+				line.append(c);
+
+				if (cr && c == '\n')
+					break;
+
+				if (c == '\r') {
+					cr = true;
+				} else {
+					cr = false;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.print(line.toString());
+
+		return line.toString();
+	}
+	*/
 }
--- VSP/VSP3_Middleware/src/lpon/name_service/LookUpTable.java
+++ VSP/VSP3_Middleware/src/lpon/name_service/LookUpTable.java
@@ -1,9 +1,9 @@
 package lpon.name_service;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
-abstract class LookUpTable {
-	private static HashMap<String, ResolveObject> objects = new HashMap<String, ResolveObject>();
+public class LookUpTable {
+	private static ConcurrentHashMap<String, ResolveObject> objects = new ConcurrentHashMap<String, ResolveObject>();
 	
 	public static void rebind(String id, String host, int port) {
 		objects.put(id, new ResolveObject(host, port, id));
