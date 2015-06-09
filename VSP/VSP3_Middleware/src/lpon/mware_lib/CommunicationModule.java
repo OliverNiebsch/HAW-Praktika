1aba7a33ae9cd0293ab6a44528d10e9d09df54e9
Lars.Porep@porep.de <Lars.Porep@porep.de>
2015-06-09 17:04:55 +0400

    CommunicationModule : remote callMethod - object stream

--- VSP/VSP3_Middleware/src/lpon/mware_lib/CommunicationModule.java
+++ VSP/VSP3_Middleware/src/lpon/mware_lib/CommunicationModule.java
@@ -1,22 +1,57 @@
 package lpon.mware_lib;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class CommunicationModule {
 	private static int nsPort;
 	private static String nsHost;
-	
-	public static void init(String nsHost, int nsPort){
+
+	public static void init(String nsHost, int nsPort) {
 		CommunicationModule.nsPort = nsPort;
 		CommunicationModule.nsHost = nsHost;
 	}
-	
+
 	/**
 	 * 
 	 * @param objectID
 	 * @param MethodName
 	 */
 	public MessageReply callMethod(MessageCall messageCall, String host, int port) {
-		// TODO - implement CommunicationModule.callMethod
-		throw new UnsupportedOperationException();
+		Socket con = null;
+		try {
+			con = new Socket(host, port);
+			ObjectOutputStream writer = new ObjectOutputStream(
+					con.getOutputStream());
+			ObjectInputStream reader = new ObjectInputStream(
+					con.getInputStream());
+			writer.writeObject(messageCall);
+			writer.flush();
+
+			Object rawReply = null;
+			try {
+				rawReply = reader.readObject();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			if (!(rawReply instanceof MessageReply)) {
+				// Communication der Middleware mit sich selbst ist nicht
+				// correct.
+				throw new RuntimeException("Ungültiges Reply Object erhalten");
+			}
+			MessageReply messageReply = (MessageReply) rawReply;
+			con.close();
+			return messageReply;
+		} catch (UnknownHostException e) {
+			e.printStackTrace();// TODO Auto-generated catch block
+		} catch (IOException e) {
+			e.printStackTrace();// TODO Auto-generated catch block
+		}
+		return null;
 	}
 
 	/**
@@ -24,7 +59,7 @@ public class CommunicationModule {
 	 * @param servant
 	 * @param name
 	 */
-	public void rebindObject(Object servant, String name) {
+	public void rebindObject(Object servant, String name, String host, int port) {
 		// TODO - implement CommunicationModule.rebindObject
 		throw new UnsupportedOperationException();
 	}
