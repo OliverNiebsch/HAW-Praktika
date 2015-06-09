14447a834931a92074077f9463361a0ab057518d
OliverNiebsch <oliver.niebsch@haw-hamburg.de>
2015-06-09 18:57:43 +0200

    Serverimplementation weiter gemacht

--- VSP/VSP3_Middleware/src/lpon/testcase/server/Server.java
+++ VSP/VSP3_Middleware/src/lpon/testcase/server/Server.java
@@ -1,13 +1,29 @@
 package lpon.testcase.server;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import lpon.mware_lib.NameService;
 import lpon.mware_lib.ObjectBroker;
 
 public class Server {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		AccessorOneClassOne a1c1 = new AccessorOneClassOne();
 		AccessorOneClassTwo a1c2 = new AccessorOneClassTwo();
 		AccessorTwoClassOne a2c1 = new AccessorTwoClassOne();
+		
+		ObjectBroker mware = ObjectBroker.init("141.22.31.178", 13037, true);
+		NameService nService = mware.getNameService();
+		
+		nService.rebind(a1c1, "viiktor");
+		nService.rebind(a1c2, "oliver");
+		nService.rebind(a2c1, "lars");
+		
+		InputStreamReader reader = new InputStreamReader(System.in);
+		reader.read();
+		
+		mware.shutDown();
 	}
 
 }
