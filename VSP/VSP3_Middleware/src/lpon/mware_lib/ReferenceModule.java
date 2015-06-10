a786e6f04e18a1f058680fbd43fe031bc67d85a1
Lars.Porep@porep.de <Lars.Porep@porep.de>
2015-06-10 13:09:42 +0200

    ReferenceModule referenzen update

--- VSP/VSP3_Middleware/src/lpon/mware_lib/ReferenceModule.java
+++ VSP/VSP3_Middleware/src/lpon/mware_lib/ReferenceModule.java
@@ -5,11 +5,11 @@ import java.util.concurrent.ConcurrentHashMap;
 public abstract class ReferenceModule {
 	private static ConcurrentHashMap<String, Object> objects = new ConcurrentHashMap<String, Object>();
 	
-	protected static void addObject(String id, Object obj) {
-		objects.put(id, obj);
+	protected static void addObject(String id, Object obj) { //REF 1.1 @ rebind
+		objects.put(id, obj); 
 	}
 	
-	protected static Object getObject(String id) {
-		return objects.get(id);
+	protected static Object getObject(String id) { //REF 2.1.1 @ rmi
+		return objects.get(id); //REF 2.1.2
 	}
 }
