371636d92529dcd3fb61679a4a981c96a24bbc62
Lars.Porep@porep.de <Lars.Porep@porep.de>
2015-06-17 11:57:10 +0200

    Unklare config files hinzugefügt in Fertigungsauftrag,

---
+++ mps.fertigung/src/main/java/lpon/mps/fertigungsauftrag/config/AppConfiguration.java
@@ -0,0 +1,13 @@
+package lpon.mps.fertigungsauftrag.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Base configuration of Application referenced in web.xml or via spring setup
+ *
+ */
+@Configuration
+@ComponentScan(basePackages = "lpon.mps")
+public class AppConfiguration {
+}
