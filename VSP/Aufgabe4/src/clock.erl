562c88195d9d514d1f2e21f3d2b3bb6c6b3de24a
Lars.Porep@porep.de <Lars.Porep@porep.de>
2015-06-30 18:54:41 +0200

    Clock überarbeitet

--- VSP/Aufgabe4/src/clock.erl
+++ VSP/Aufgabe4/src/clock.erl
@@ -2,19 +2,60 @@
 -import(werkzeug, [get_config_value/2,logging/2,logstop/0,openSe/2,openSeA/2,openRec/3,openRecA/3,createBinaryS/1,createBinaryD/1,createBinaryT/1,createBinaryNS/1,concatBinary/4,message_to_string/1,shuffle/1,timeMilliSecond/0,reset_timer/3,compareNow/2,getUTC/0,compareUTC/2,now2UTC/1,type_is/1,to_String/1,bestimme_mis/2,testeMI/2]).
 
 %% API
--export([initClock/0, getTimespanToSlot/2, getCurFrame/1, getCurSlot/1, getCurrentTimeInSlot/2]).
+-export([initClock/2, startSendTimer/2, startFrameTimer/2, getCurFrame/1, getCurSlot/1, getCurrentTimeInSlot/2]).
 
 %% Schnittstellen
 
 %% Initialisierung
 % erzeugt eine neue Clock
-initClock(Offset) ->
-  [Offset,PID_Receive].
+initClock(Offset, PID_Receive) ->
+  Clock = {Offset,PID_Receive, nil, nil},
+  
+  ClockNeu = startFrameTimer(Clock),
+  
+  ClockNeu.
+
+startSendTimer(Clock, Slot, Frame) when (Slot > 0 and Slot < 26) ->
+	% TODO Alten Timer killen
+	{Offset, PID_Receive, FrameTimerPID, _} = Clock,
+	
+	Timeout = getTimespanToSlot(Clock, Slot, Frame),
+	SendTimerPID = initTimer(Timeout, PID_Receive, sendTimer),
+	
+	{Offset, PID_Receive, FrameTimerPID, {SendTimerPID, Slot, Frame}}.
+  
+startFrameTimer(Clock) ->
+	% TODO Alten Timer killen
+	{Offset, PID_Receive, _, SendTimer} = Clock,
+	CurrentTime = getCurrentTime(Clock),
+	
+	Timeout = 1000-(CurrentTime rem 1000),
+	
+	FrameTimerPID = initTimer(Timeout + 10, PID_Receive, frameTimer),
+	{Offset, PID_Receive, FrameTimerPID, SendTimer}.
+  
+setTimer(Pid, TimeMS, TimeoutReplyMsg) ->
+  receive
+	{sync, Timeout} when Timeout > 0 ->
+		setTimer(Pid, Timeout, TimeoutReplyMsg);
+	{sync, Timeout} ->
+		Pid ! TimeoutReplyMsg;
+    kill ->
+      true
+  after
+    TimeMS ->
+      Pid ! TimeoutReplyMsg
+  end.
+  %notimeout / timeout
+  %setTimer(Pid, TimeMS, TimeoutReplyMsg). %Restart timer
+
+initTimer(Timeout, PID_Receive, Message) ->
+  spawn(clock, setTimer, [PID_Receive, Timeout, Message]).
 
 %% Getter
 % Senden(Kollision) - 1.6: liefert die Zeitspanne in Millisekunden bis zum angegebenen Slot
 getTimespanToSlot(Clock, FreeSlot, Frame) ->
-	(getCurrentTime - calculateTimeBySlot).
+	(getCurrentTime(Clock) - calculateTimeBySlot(FreeSlot, Frame).
 
 %Liefert den Zeitpunkt andem ein bestimmter Slot beginnt als Timestamp in MS
 calculateTimeBySlot(Slot, Frame) ->
@@ -29,7 +70,7 @@ getCurSlot(Clock) ->
   getSlotByTime(getCurrentTime(Clock)).
 
 getCurrentTime(Clock) ->
-	[Offset,_PID_Receive] = Clock,
+	{Offset,PID_Receive, _FrameTimerPID, _SendTimerPID} = Clock,
 	werkzeug:getUTC() + Offset.
   
 % Senden - 1.6: liefert die aktuelle Zeit, wenn der angegebene Slot noch aktiv ist
@@ -48,34 +89,28 @@ getSlotByTime(Timestamp) ->
 
 getFrameByTime(Timestamp) ->
 	Timestamp div 1000.
-
+	
 %Sync mit anderem Timestamp
 synchronize(Clock, Timestamp) ->
+	{Offset, _PID_Receive, _FrameTimerPID, _SendTimerPID} = Clock,
+	
 	Differenz = Timestamp - (getCurrentTime(Clock)),
-	NewOffset = Differenz/2,
+	HalfDiff = (Differenz div 2),
+	NewOffset = Offset + HalfDiff, %Floor - dif = 1  === ?
+	setTimerSyncDrift(Clock, HalfDiff), % PUSH MSG TO TIMERS
 	setOffset(Clock, NewOffset).
-
+	
+setTimerSyncDrift(Clock, OffsetDiff) ->
+	{_Offset, _PID_Receive, FrameTimerPID, {SendTimerPID, Slot, Frame}} = Clock,
+	CurrentTime = getCurrentTime(Clock),
+	
+	NewFrameTimeout = 1000-(CurrentTime rem 1000) - OffsetDiff, %WIRD NEGATIV; WENN FRAME GRENEZ DURCH ANPASSUNG ÜBERSPRUNGEN
+	FrameTimerPID ! {sync, NewFrameTimeout},
+	
+	NewSendTimeout = getTimespanToSlot(Clock, Slot, Frame)
+	SendTimerPID ! {sync, NewFrameTimeout}.
+	
+	
 setOffset(Clock, NewOffset) ->
-	[Offset,PID_Receive] = Clock,
-	[NewOffset, PID_Receive].
-	
-
-	
-resetTimer(TimerPID) ->
-  TimerPID ! resettimer.
-
-setTimer(Pid, TimeMS, TimeoutReplyMsg) ->
-  receive
-    resettimer ->
-      setTimer(Pid, TimeMS, TimeoutReplyMsg);
-    kill ->
-      true
-  after
-    TimeMS ->
-      Pid ! TimeoutReplyMsg
-  end,
-  %notimeout / timeout
-  %setTimer(Pid, TimeMS, TimeoutReplyMsg). %Restart timer
-
-initTimer(HalfTimeoutInMS) -> %INIT WITH timeout/2
-  spawn(ggt, setTimer, [self(), HalfTimeoutInMS, timeoutMessage]).
+	{_Offset, PID_Receive, FrameTimerPID, SendTimer} = Clock,
+	{NewOffset, PID_Receive, FrameTimerPID, SendTimer}.
