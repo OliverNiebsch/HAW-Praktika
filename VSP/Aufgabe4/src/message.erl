b28c45e716cf31d666ab004ec2a0c17da7451434
Lars.Porep@porep.de <Lars.Porep@porep.de>
2015-06-30 15:36:46 +0200

    Message update

--- VSP/Aufgabe4/src/message.erl
+++ VSP/Aufgabe4/src/message.erl
@@ -1,7 +1,7 @@
 -module(message).
 
 %% API
--export([binaryToMessageObj/1, newMessage/1, getFrame/1, getStation/1, setNextSlot/2, setTime/2]).
+-export([packetToMessageObj/1, newMessage/5, getFrame/1, getStation/1, getStationTyp/1, getTime/1,  setNextSlot/2, setTime/2]).
 
 %% Schnittstellen
 
@@ -23,18 +23,26 @@ getFrame(Message) ->
 	TargetFrame.
 
 % Empfang - alt: gibt die Station an, von der die Message gesendet wurde/wird
+getStation(Message) ->
+  {_TargetFrame, _StationTyp,Nutzdaten,_Slot,_Timestamp} = Message,
+	%Nutzdaten 1-10 = Name der Station
+	%{Stationsname, _} = lists:split(10, Nutzdaten), %Alternative Variante
+	Stationsname = lists:sublist(Nutzdaten, 10),
+	Stationsname.
+	
+
 getStationTyp(Message) ->
   {_TargetFrame, StationTyp,_Nutzdaten,_Slot,_Timestamp} = Message,
 	StationTyp.
-
+	
 getTime(Message) ->
 	{TargetFrame, StationTyp,Nutzdaten,Slot,Timestamp},
 	Timestamp.
 	
 %% Setter
 % Senden - 1.3: setzen des Slots, in dem die Station dieser Nachricht im nächsten Frame senden will
-setNextSlot(Message, Slot) ->
-	{TargetFrame, StationTyp, Nutzdaten, _, Timestamp} = Message,
+setNextSlot(Message, Slot, TargetFrame) ->
+	{_, StationTyp, Nutzdaten, _, Timestamp} = Message,
 	{TargetFrame, StationTyp, Nutzdaten,  Slot, Timestamp}.
 
 % Senden - 2: setzt die Sendezeit der Nachricht
