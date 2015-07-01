f3b74d9fe2863c3e27f9d40953619f310b74fd11
OliverNiebsch <oliver.niebsch@haw-hamburg.de>
2015-07-01 10:07:25 +0200

    Start auf einen Parameter umgemünzt (Liste)

--- VSP/Aufgabe4/src/receiver.erl
+++ VSP/Aufgabe4/src/receiver.erl
@@ -2,14 +2,14 @@
 -import(werkzeug, [get_config_value/2, logging/2, logstop/0, openSe/2, openSeA/2, openRec/3, openRecA/3, createBinaryS/1, createBinaryD/1, createBinaryT/1, createBinaryNS/1, concatBinary/4, message_to_string/1, shuffle/1, timeMilliSecond/0, reset_timer/3, compareNow/2, getUTC/0, compareUTC/2, now2UTC/1, type_is/1, to_String/1, bestimme_mis/2, testeMI/2]).
 
 %% API
--export([start/4]).
+-export([start/1]).
 
 %% Schnittstellen
 
 %% MAIN
 % startet und initialisiert das ReceiveModul und alle benoetigten anderen Module
 
-start(WadisMeeep, Port, StationTyp, ClockOffset) ->
+start({WadisMeeep, Port, StationTyp, ClockOffset]) ->
   Socket = openRec({142,22,78,197}, WadisMeeep, Port),
   gen_udp:controlling_process(Socket, self()),% diesen Prozess PidRec (als Nebenlaeufigenprozess gestartet) bekannt geben mit
 
