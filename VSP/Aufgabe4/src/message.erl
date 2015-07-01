8e8242270e66ce7dcaa186ed4af2d4796a678dfd
OliverNiebsch <oliver.niebsch@haw-hamburg.de>
2015-07-01 12:08:46 +0200

    Debug Code

--- VSP/Aufgabe4/src/message.erl
+++ VSP/Aufgabe4/src/message.erl
@@ -61,5 +61,6 @@ setData(Message, Data) ->
 	
 messageToBinary(Message) ->
 	{_TargetFrame, StationTyp, Nutzdaten, Slot, Timestamp} = Message,
-  logging("message.log", "Wandle Message in binray um, mit Daten: " ++ to_String(StationTyp) ++ ", " ++ to_String(Nutzdaten) ++ ", " ++ to_String(Slot) ++ ", " ++ to_String(Timestamp)),
+  logging("message.log", "Wandle Message in binray um, mit Daten: " ++ to_String(StationTyp) ++ ", " ++ to_String(Nutzdaten) ++ ", " ++ to_String(Slot) ++ ", " ++ to_String(Timestamp) ++ "\n"),
+  timer:sleep(2000), %DEBUG
   concatBinary(createBinaryS(StationTyp), createBinaryD(Nutzdaten), createBinaryNS(Slot), createBinaryT(Timestamp)).
